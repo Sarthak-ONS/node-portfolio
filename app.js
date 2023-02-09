a12425926ceabab25c6a4bb6c4321fbbf1ac544c
Sarthak Agarwal <57105611+Sarthak-ONS@users.noreply.github.com>
2023-02-09 13:38:34 +0530

    Added Home Page

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
 const PORT = process.env.PORT | 4000
 
-app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 
 app.get('/', (req, res, next) => {
-    res.render('index')
+    console.log("req on home");
+    res.render('home', {
+        pageTitle: 'Sarthak Agarwal', path: '/'
+    })
 })
 
 app.listen(PORT, () => {
---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+const backdrop = document.querySelector('.backdrop');
+const sideDrawer = document.querySelector('.mobile-nav');
+const menuToggle = document.querySelector('#side-menu-toggle');
+
+function backdropClickHandler() {
+  backdrop.style.display = 'none';
+  sideDrawer.classList.remove('open');
+}
+
+function menuToggleClickHandler() {
+  backdrop.style.display = 'block';
+  sideDrawer.classList.add('open');
+}
+
+backdrop.addEventListener('click', backdropClickHandler);
+menuToggle.addEventListener('click', menuToggleClickHandler);
+
+
+function openCV() {
+  console.log("Function is called");
+  var pdfUrl = "../Sarthak_Agarwal.pdf";
+  window.open(pdfUrl, '_blank')
+}
